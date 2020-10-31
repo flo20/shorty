69ccc8fa5129c77b0d897f5ab17f98addf4f95d5
flo20 <59807384+flo20@users.noreply.github.com>
2020-10-31 09:31:09 +0200

    Built server side, creating client side

--- shorten-link-app/server.js
+++ shorten-link-app/server.js
@@ -3,8 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser"); //bodyparser middleware
 
-
-
 //Initialize
 const app = express();
 
@@ -31,9 +29,19 @@ app.use("/api/redirect", redirect);
 
 //hash route
 app.get("/:hash", (req, res) => {
-    const id = req.params.hash;
-    console.log(id);
-})
+  const id = req.params.hash;
+  //console.log(id);
+  URL.findOne({ _id: id }, (err, doc) => {
+    if (doc) {
+      console.log(doc.url);
+      res.redirect("http://" + doc.url);
+    } else {
+      console.log(err);
+      res.status(400).json({ err: "Link has expired." });
+      res.redirect("/");
+    }
+  });
+});
 
 //Listening path
 app.get("/login", (req, res) => {
