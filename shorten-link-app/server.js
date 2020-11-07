a142faf611a62cd271ff96e8c2894459475f5ede
flo20 <59807384+flo20@users.noreply.github.com>
2020-11-07 21:26:14 +0200

    Deploys

--- shorten-link-app/server.js
+++ shorten-link-app/server.js
@@ -3,6 +3,9 @@ const app = express(); //Initialize
 require("./config/db")(); //calling database
 require("./startup/routes")(app); // initializing routes
 
+
+
+
 //Port
 const port = process.env.PORT || 5000;
 
--- shorten-link-app/startup/routes.js
+++ shorten-link-app/startup/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const shortLink = require("../routes/shortLink");
 const redirect = require("../routes/redirect");
+const path = require("path");
 
 module.exports = function (app) {
   app.use(express.json());
@@ -11,4 +12,14 @@ module.exports = function (app) {
   //routes
   app.use("/links", shortLink);
   app.use("/", redirect);
+
+  // Serve static assets if in production
+  if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+    });
+  }
 };
